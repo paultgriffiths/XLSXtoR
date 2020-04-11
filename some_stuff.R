10be7eecfe521986c8de729eeabb5860b1cf24ac
Paul Griffiths <2770885+paultgriffiths@users.noreply.github.com>
2020-04-11 09:54:39 +0100

    update code with comments

--- some_stuff.R
+++ some_stuff.R
@@ -1,27 +1,39 @@
 require(here)
-require(here)
 
+# sample data file
 test.data <- readxl::read_xlsx(here::here('data/sample-xlsx-file-for-testing.xlsx'))
+
+# show basic structure of the dataframe just created
 head(test.data)
 
+# how you take the mean of a dataframe column, indexing using $NAME (or by column number, see below)
 print(c('Mean of manufacturing price is',mean(test.data$`Manufacturing Price`)))
 
-# getting date info from string data
+# getting date info from string data - R is the new overlord and dates must be posix compliant
 test.data$newMonths <- as.Date.POSIXct(test.data$Date, format='%Y-%M-%d')
 
 # convert to human readable form, say you just wanted to sort by months
-# here %b is month as a 3-letter string
+# here %b is month as a 3-letter string, and format prints out the date in that format
 # in R indices start as 1, and square brackets index the array
+# just the first three rows of the Date column
 format(as.Date.POSIXct(test.data$Date[1:3], format='%Y-%M-%d'), '%b')
 
 # if I was starting now I'd probably just start with dplyr as the syntax is simpler and
 # it's just one library to use.  you just chain operations together using dplyr pipes (%>%)
+# operations read left to right as well
 require(dplyr)
 
 # tests for if the data are numbers, then takes mean
+# NB this doesn't generate a new data frame, just outputs the operation
 test.data %>% summarise_if(is.numeric, mean)
 
 # select on month and year
+# and pass into new data frame
 data.June.2014 <- test.data %>% filter(Year ==2014, `Month Number`==6)
+
+# can also use
 head(data.June.2014)
 tail(data.June.2014)
+
+# or just say fun it and tell me everything
+summary(data.June.2014)
